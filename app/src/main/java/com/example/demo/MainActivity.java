a65414a59444e1d7aaf482fbfcd5906f45cc1567
Jonas <jonasdhein@gmail.com>
2021-05-27 20:33:51 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text, text2;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView);
+        text2 = findViewById(R.id.textView2);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                OperacaoTask op1 = new OperacaoTask(text);
+                op1.execute(100);
+
+                OperacaoTask op2 = new OperacaoTask(text);
+                op2.execute(300);
+            }
+        });
+
+
+
+    }
+}
